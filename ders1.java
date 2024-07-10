8c412156fc92df7937c2ad182b7ecfeeafa30827
Nergiz Alıci <159393297+nergizal@users.noreply.github.com>
2024-07-10 19:54:46 +0300

    Add files via upload

---
+++ ders1.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //Practice 1:
+        /*
+        double num = 163.4;
+        double num2 = num*num;
+        System.out.printf("%.2f\n",num2);
+        System.out.printf("%+.2f\n",num2);
+        System.out.printf("%-10f\n",num2);
+        System.out.printf("%-15.3f\n",num2);
+        System.out.printf("%20.3f\n",num2);
+        System.out.printf("%,.4f\n",num2);
+        System.out.printf("%015.3f\n",num2);
+        
+        //2. Practice
+        Scanner num1=new Scanner(System.in);
+        Scanner num3=new Scanner(System.in);
+        System.out.println("Plese enter 2 number: ");
+        int n1= num1.nextInt();
+        int n3= num3.nextInt();
+        int num2 = n1 + n3;
+
+        System.out.printf("%015d\n",num2);
+        System.out.printf("%,15d\n",num2);
+        System.out.printf("%-,15d\n",num2);
+        System.out.printf("%+12d\n",num2);
+        System.out.printf("-%,d\n",num2);
+*/
+System.out.printf("%015d\n",num2);
+}
+
+    }
